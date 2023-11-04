6ce49056f029a65dcbccb6bd0c9a1ee1500997c1
daveinthahood <davidesimone96@outlook.it>
2023-11-04 13:07:24 +0100

    ADD: Card File and Component Folder

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div>
       <h1 className='w-full font-extrabold'> Ciao </h1>
+      <h3 className='w-full bg-black text-white'> Menel Clown </h3>
     </div>
   )
 }
